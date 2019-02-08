e9125f4a9976139ac9f34358c4055fd0ced02423
Anxin <anxinyang114@gmail.com>
2019-02-07 17:34:17 -0800

    cc

--- service.js
+++ service.js
@@ -1,12 +1,21 @@
 const assets = [
     './',
     './main.js',
-    './service.js'
+    './service.js',
+    './manifest.json',
+    './images/icons/icon-72x72.png',
+    './images/icons/icon-96x96.png',
+    './images/icons/icon-128x128.png',
+    './images/icons/icon-144x144.png',
+    './images/icons/icon-152x152.png',
+    './images/icons/icon-192x192.png',
+    './images/icons/icon-384x384.png',
+    './images/icons/icon-512x512.png',
 ];
 
 
 self.addEventListener('install', async function () {
-    console.log('Install event at '+ Date.now();
+    console.log('Install event at '+ Date.now());
     const cache = await caches.open('assets');
     cache.addAll(assets);
 
@@ -15,10 +24,33 @@ self.addEventListener('install', async function () {
 self.addEventListener('fetch', function (e) {
     console.log('Fetch event at' + + Date.now());
     const req = e.request;
-    e.respondWith(cacheFirst(req));
+    const url = new URL(req.url);
+    if(url.origin === location.origin){
+        e.respondWith(cacheFirst(req));
+    }else{
+        e.respondWith(networkFirst(req));
+    }
+
 });
 
 async  function cacheFirst(req) {
     const cachedResponse = await caches.match(req);
+    console.log('Requested ' + req.url + ' response with ' + (cachedResponse? 'cache':'fetch') + 'data');
+    return cachedResponse || fetch(req);
+}
+
+async  function networkFirst(req) {
+    const cache = await caches.open('data');
+
+    try{
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    }catch(e){
+        return await cache.match(req);
+    }
+
+    const cachedResponse = await caches.match(req);
+    console.log('Requested ' + req.url + ' response with ' + (cachedResponse? 'cache':'fetch') + 'data');
     return cachedResponse || fetch(req);
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ window.addEventListener('resize', function () {
     cc.updateValue('viewport', {width: window.innerWidth, height: window.innerHeight});
 });
 
+
+start();
+
+
+
+
+
+
+//==== functions
 function setupSW() {
     if('serviceWorker' in navigator) {
         console.log('Service worker supported!');
@@ -23,7 +32,192 @@ function setupSW() {
 function start() {
     setupSW();
     let root = cc.select('#body');
-    
-
+    let app = root.appendChild(cc.createElement('div', 'app'));
+    app.css({
+        background: 'rgb(32, 32, 32)',
+        width: '100vw',
+        height: '100vh',
+        padding: '32px 16px',
+        display: 'flex',
+        flexDirection: 'column'
+    });
+    renderBar(app);
+    renderRecords(app);
+    renderFooter(app);
 }
-start();
+
+function renderBar(app){
+    let graphContainer = app.add('div')
+        .css({
+            position: 'relative',
+            width: '100%',
+            height: '64px',
+            padding: '8px',
+            background: 'rgb(64, 64, 64)',
+            color: 'rgba(255,255,255, 0.8)',
+            fontSize: '10px',
+            boxShadow: 'rgb(16, 16, 16) 4px 4px 12px'
+        });
+    graphContainer.add('span')
+        .content('Spend vs. Income');
+    let barContainer = graphContainer.add('div').css({
+        position: 'relative',
+        height: '24px',
+        width: '100%',
+        background: 'black',
+        marginTop: '4px',
+        borderRadius: '25px',
+        overflow:'hidden'
+    });
+
+    barContainer.add('div', 'incomeBar')
+        .css({
+            position: 'absolute',
+            height: '24px',
+            width: '100%',
+            background: '#4cd137',
+            top: '0',
+            left: '0'
+        });
+    barContainer.add('div', 'spendBar')
+        .css({
+            position: 'absolute',
+            height: '24px',
+            width: '50%',
+            background: '#e84118',
+            top: '0',
+            left: '0'
+        });
+}
+
+function renderRecords(app){
+    let container = app.add('div')
+        .css({
+            position: 'relative',
+            width: '100%',
+            padding: '8px',
+            background: 'rgb(64, 64, 64)',
+            color: 'rgba(255,255,255, 0.8)',
+            fontSize: '10px',
+            boxShadow: 'rgb(16, 16, 16) 4px 4px 12px',
+            marginTop: '8px',
+            flexGrow:1,
+            overflow: 'auto'
+        });
+    container.add('span')
+        .content('History');
+    container.add('div')
+        .css({
+            marginTop: '4px'
+        })
+        .bind('history', function (d = []) {
+            let self = this;
+            self.removeAllChildren();
+            d.forEach(function (d) {
+                self.addElement(renderRecord(d))
+            });
+        });
+    cc.setValue('history', [
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+        {type: 1, category: 'Salary', amount: 1000, date: new Date()},
+
+
+        ])
+}
+
+function renderRecord(d) {
+    let {type, category, amount, date, comment} = d;
+    let block = cc.createElement('div')
+        .addClass(type)
+        .addClass(category)
+        .addClass(date.getMonth())
+        .addClass(date.getYear())
+        .css({
+            position: 'relative',
+            width: '100%',
+            padding: '8px',
+            background: 'rgb(32, 32, 32)',
+            color: 'rgba(255,255,255, 0.8)',
+            fontSize: '12px',
+            marginBottom: '4px',
+        });
+    block.add('span')
+        .css({
+            display: 'inline-block',
+        })
+        .content(category);
+
+    block.add('strong')
+        .css({
+            display: 'inline-block',
+            color: type === 0 ?'red':'green',
+            float: 'right'
+        })
+        .content(amount);
+
+    block.add('br');
+    block.add('span')
+        .css({
+            display: 'inline-block',
+            fontSize: '8px',
+            color: 'rgb(128, 128, 128)'
+        })
+        .content(date.toString().substring(0, date.toString().indexOf('GMT')-1));
+
+    block.add('span')
+        .css({
+            display: 'inline-block',
+            fontSize: '8px',
+            color: 'rgb(128, 128, 128)',
+            float: 'right',
+            paddingTop: '4px'
+        })
+        .content(comment || 'No Comment');
+
+    return block
+}
+
+function renderFooter(app){
+    let container = app.add('div')
+        .css({
+            position: 'relative',
+            width: '100%',
+            height: '64px',
+            padding: '8px',
+            background: 'rgb(64, 64, 64)',
+            color: 'rgba(255,255,255, 0.8)',
+            fontSize: '10px',
+            boxShadow: 'rgb(16, 16, 16) 4px 4px 12px',
+            marginTop: '8px'
+        });
+}
